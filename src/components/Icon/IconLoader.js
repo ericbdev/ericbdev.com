e33f22d615c7e3d27958f1822a35db797359f3ed
Eric Bright <ericbdev@gmail.com>
2021-04-11 21:18:16 -0400

    assertive fallback loader

--- src/components/Icon/IconLoader.js
+++ src/components/Icon/IconLoader.js
@@ -1,28 +1,50 @@
 import React, { memo, useState, useEffect, useRef } from 'react';
 
-const IconLoader = ({ name, size = 'initial', title, ...rest }) => {
+const Loader = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="100"
+    height="100"
+    viewBox="0 0 100 100"
+    fill="currentColor"
+  >
+    <text x="10" y="10">
+      Loading...
+    </text>
+  </svg>
+);
+
+const IconLoader = ({ name, size = 'initial', title: _title, ...rest }) => {
   const ImportedIconRef = useRef(null);
+  const title = _title || name;
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     setLoading(true);
     (async () => {
       try {
-        // https://react-svgr.com/docs/options/
-        const [named, at12, at16, at24] = await Promise.allSettled([
-          import(`!!@svgr/webpack?-svgo,+memo,+titleProp,+ref!./svgs/${name}.svg`),
-          import(`!!@svgr/webpack?-svgo,+memo,+titleProp,+ref!./svgs/${name}-12.svg`),
-          import(`!!@svgr/webpack?-svgo,+memo,+titleProp,+ref!./svgs/${name}-16.svg`),
-          import(`!!@svgr/webpack?-svgo,+memo,+titleProp,+ref!./svgs/${name}-24.svg`),
+        const [named, at12, at16, at28] = await Promise.allSettled([
+          import(
+            `!!@svgr/webpack?-svgo,+memo,+titleProp,+ref!./svgs/${name}.svg`
+          ),
+          import(
+            `!!@svgr/webpack?-svgo,+memo,+titleProp,+ref!./svgs/${name}-12.svg`
+          ),
+          import(
+            `!!@svgr/webpack?-svgo,+memo,+titleProp,+ref!./svgs/${name}-16.svg`
+          ),
+          import(
+            `!!@svgr/webpack?-svgo,+memo,+titleProp,+ref!./svgs/${name}-28.svg`
+          ),
         ]).then(values =>
           values.map(v => (v.status === 'fulfilled' ? v : null)),
         );
 
         const weighted = {
           initial: named,
-          12: at12 || at16 || at24 || named,
-          16: at16 || at24 || named || at12,
-          24: at24 || named || at16 || at12,
+          12: at12 || at16 || at28 || named,
+          16: at16 || at28 || named || at12,
+          28: at28 || named || at16 || at12,
         };
 
         ImportedIconRef.current =
@@ -37,10 +59,11 @@ const IconLoader = ({ name, size = 'initial', title, ...rest }) => {
 
   if (!loading && ImportedIconRef.current) {
     const { current: ImportedIcon } = ImportedIconRef;
-    return <ImportedIcon title={title || name} {...rest} />;
-  }
 
-  return null;
+    return <ImportedIcon title={title} {...rest} />;
+  }
+  
+  return <Loader />;
 };
 
 export default memo(IconLoader);
